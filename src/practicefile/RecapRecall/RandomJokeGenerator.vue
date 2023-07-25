87ede5517f62c6697105066cf58a15e542386cb5
Chandan Ojha <chandanojha776@gmail.com>
2023-07-25 11:10:57 +0600

    updated

---
+++ src/practicefile/RecapRecall/RandomJokeGenerator.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const joke = reactive({
+  value: "Chuck Norris can do a wheelie on a unicycle.",
+});
+
+function getJoke() {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      joke.value = data.value;
+    });
+}
+</script>
+
+<template>
+  <section class="flex flex-col items-center w-[500px]">
+    <p>
+      {{ joke.value }}
+    </p>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-48 mt-5"
+      @click="getJoke()"
+    >
+      Get a new joke
+    </button>
+  </section>
+</template>
+
+<style scoped></style>
