1b845081433eae7bafabffc02fc4cc9a4fb11573
Chandan Ojha <chandanojha776@gmail.com>
2023-08-09 20:41:23 +0600

    updated

--- lifecycle-hooks/src/App.vue
+++ lifecycle-hooks/src/App.vue
@@ -1,37 +1,5 @@
-<script setup>
-import { ref, onUpdated } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-const message = ref("Hello World!");
-onUpdated(() => {
-  console.log("App.vue updated!");
-});
-</script>
+<script setup></script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <h1>{{ message }}</h1>
-  <button @click="message = 'Hello From Ostad'">Update Message</button>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<template></template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- lifecycle-hooks/src/components/HelloWorld.vue
+++ lifecycle-hooks/src/components/HelloWorld.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onUpdated } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+onUpdated(() => {
+  console.log("HelloWorld.vue updated");
+});
+
+const count = ref(0);
 </script>
 
 <template>
