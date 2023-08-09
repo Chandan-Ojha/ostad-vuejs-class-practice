cc3c8da50c4700de5f980b45dfc37b63a3a40661
Chandan Ojha <chandanojha776@gmail.com>
2023-08-09 20:06:33 +0600

    `updated`

---
+++ lifecycle-hooks/src/practicefile/hooks.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, reactive } from "vue";
+const index = ref(0);
+const compositionHooks = [
+  "onBeforeMount",
+  "onMounted",
+  "onBeforeUpdate",
+  "onUpdated",
+  "onBeforeUnmount",
+  "onUnmounted",
+  "onErrorCaptured",
+];
+
+function nextHook() {
+  if (index.value < compositionHooks.length) {
+    index.value++;
+  }
+}
+</script>
+
+<template>
+  <section class="mx-auto container">
+    <h1 class="mt-10 mb-20">Lifecycle Hooks</h1>
+
+    <h2 class="my-5 text-2xl" v-for="n in index">
+      {{ compositionHooks[n - 1] }}
+    </h2>
+
+    <button class="">
+      <button
+        @click="nextHook()"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Next Hook
+      </button>
+    </button>
+  </section>
+</template>
+
+<style scoped></style>
