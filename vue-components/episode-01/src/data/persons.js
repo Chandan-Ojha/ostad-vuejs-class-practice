b4fff5094b2ce7f0461b2319b7318507fe6673db
Chandan Ojha <chandanojha776@gmail.com>
2023-08-11 20:54:15 +0600

    `episode-01` practice done

---
+++ vue-components/episode-01/src/data/persons.js
@@ -0,0 +1,40 @@
+const persons = [
+  {
+    name: "Bonnie Green",
+    designation: "CEO & Web Developer",
+    avatar:
+      "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+    about:
+      " Bonnie drives the technical strategy of the flowbite platform and brand.",
+    age: 30,
+  },
+  {
+    name: "Jese Leos",
+    designation: "CTO",
+    avatar:
+      "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png",
+    about:
+      " Jese drives the technical strategy of the flowbite platform and brand.",
+    age: 25,
+  },
+  {
+    name: "Michael Gough",
+    designation: "Senior Front-end Developer",
+    avatar:
+      "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png",
+    about:
+      " Michael drives the technical strategy of the flowbite platform and brand.",
+    age: 22,
+  },
+  {
+    name: "Lana Byrd",
+    designation: "Marketing & Sale",
+    avatar:
+      "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png",
+    about:
+      "  Lana drives the technical strategy of the flowbite platform and brand.",
+    age: 30,
+  },
+];
+
+export { persons };
