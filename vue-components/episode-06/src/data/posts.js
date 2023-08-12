999382e1c07481fbc1e30f31305a2a5380ae7ed0
Chandan Ojha <chandanojha776@gmail.com>
2023-08-12 10:21:20 +0600

    `episode-06` practice ongoing

---
+++ vue-components/episode-06/src/data/posts.js
@@ -0,0 +1,41 @@
+const posts = [
+  {
+    title: "Deadbeef Decrementation",
+    excerpt: "How to decrement from a magic debug value to zero.",
+    tags: ["spirituality"],
+  },
+  {
+    title: "Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater",
+    excerpt:
+      "♫ Interior Crocodile Alligator, I Drive a Chevrolet Movie Theater ♫",
+    tags: ["cats"],
+  },
+  {
+    title: "Why My Practice of Thelema Makes Me Better at World of Warcraft",
+    excerpt: '"Do what thou wilt" shall be the whole of the Law.',
+    tags: ["enchiladas"],
+  },
+  {
+    title: "Shrek Predicted the COVID-19 Pandemic",
+    excerpt: "Find out how Shrek is disrupting astrology.",
+    tags: [
+      {
+        name: "orcas",
+        slug: "orcas",
+        color: "blue",
+      },
+      {
+        name: "beer",
+        slug: "beer",
+        color: "red",
+      },
+    ],
+  },
+  {
+    title: "Lorem Ipsum",
+    excerpt: "You know what time it is: lorem ipsum time.",
+    tags: ["stoicism"],
+  },
+];
+
+export { posts };
