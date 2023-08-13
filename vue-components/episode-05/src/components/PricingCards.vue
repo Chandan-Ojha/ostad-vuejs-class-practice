947ab11ffb131c59dff393c996f7b5e6b6031142
Chandan Ojha <chandanojha776@gmail.com>
2023-08-13 23:00:26 +0600

    `episode-05` practice done

---
+++ vue-components/episode-05/src/components/PricingCards.vue
@@ -0,0 +1,7 @@
+<script setup>
+import PricingCard from "./PricingCard.vue";
+import { cards } from "../data/card.js";
+</script>
+<template>
+  <PricingCard v-for="(card, index) in cards" :key="index" :card="card" />
+</template>
---
+++ vue-components/episode-05/src/data/card.js
@@ -0,0 +1,20 @@
+const cards = [
+  {
+    title: "Starter",
+    description: "Best option for personal use & for your next project.",
+    price: 29,
+  },
+  {
+    title: "Company",
+    description: "Relevant for multiple users, extended & premium support.",
+    price: 99,
+  },
+  {
+    title: "Enterprise",
+    description:
+      "Best for large scale uses and extended redistribution rights.",
+    price: 499,
+  },
+];
+
+export { cards };
