0753267a9a3bdfb5e40b72687d2b091225f12421
Chandan Ojha <chandanojha776@gmail.com>
2023-08-13 20:23:33 +0600

    `episode-02` practice done

---
+++ vue-components/episode-02/src/data/ticket.js
@@ -0,0 +1,24 @@
+const tickets = [
+  {
+    title: "The Magnificent Bogra",
+    description:
+      "Located in Rajshahi Division, Bogra is one of the oldest and most fascinating towns in Bangladesh",
+    image:
+      "https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg",
+    distance: "197 km",
+    location: "Dhaka",
+    reviews: "16 reviews",
+  },
+  {
+    title: "The Magnificent Sundarban",
+    description:
+      "Located in Khulna Division, Sundarban is one of the oldest and most fascinating towns in Bangladesh",
+    image:
+      "https://i.ibb.co/Kr4b0zJ/152013403-10158311889099633-8423107287930246533-o.jpg",
+    distance: "200 km",
+    location: "Dhaka",
+    reviews: "20 reviews",
+  },
+];
+
+export { tickets };
