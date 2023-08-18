ee3d94ee129f3e890283e9df6fe9418a0ece6cd7
Chandan Ojha <chandanojha776@gmail.com>
2023-08-18 11:03:50 +0600

    `slot` practice done

--- vue-components/component-slot/src/App.vue
+++ vue-components/component-slot/src/App.vue
@@ -1,3 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ButtonComponent from "./components/ButtonComponent.vue";
+import SlotComponent from "./components/SlotComponent.vue";
 
-<template></template>
+const msg = ref("Hello Vue 3");
+
+function clickHandlerOne() {
+  msg.value = "Click One";
+}
+
+function clickHandlerTwo() {
+  msg.value = "Click Two";
+}
+</script>
+
+<template>
+  <h2 class="text-2xl my-10">Slot</h2>
+  <p class="my-5">
+    {{ msg }}
+  </p>
+  <p>
+    <ButtonComponent :click-handler="clickHandlerOne">
+      <!-- <em class="text-orange-600">Button One</em> -->
+    </ButtonComponent>
+  </p>
+  <p class="my-5">
+    <ButtonComponent :click-handler="clickHandlerTwo">
+      <em>Button Two</em>
+    </ButtonComponent>
+  </p>
+
+  <SlotComponent>
+    Default Content 123
+    <template #header>
+      <h2 class="text-2xl my-10">This is Heading</h2>
+    </template>
+    <template v-slot:footer>
+      <h2 class="text-xl my-10">This is Footer</h2>
+    </template>
+  </SlotComponent>
+</template>
---
+++ vue-components/component-slot/src/components/SlotComponent.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <p>
+    <slot name="header">Header</slot>
+  </p>
+  <p>
+    <slot name="default">This is default content</slot>
+  </p>
+  <p>
+    <slot name="footer"></slot>
+  </p>
+</template>
