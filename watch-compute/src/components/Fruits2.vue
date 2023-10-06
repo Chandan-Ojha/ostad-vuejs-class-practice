ec31cbc25947cf1ea7314a2332d72a706a0e2718
Chandan Ojha <chandanojha776@gmail.com>
2023-10-06 15:31:32 +0600

    updated

--- watch-compute/src/components/Fruits2.vue
+++ watch-compute/src/components/Fruits2.vue
@@ -1,45 +1,58 @@
 <script setup>
 import { ref, computed } from "vue";
-const fruitsWithPrice = [
+const fruitsWithPrice = ref([
   { name: "Apple", price: 1.2 },
   { name: "Orange", price: 1.4 },
   { name: "Banana", price: 1.6 },
   { name: "Mango", price: 1.8 },
   { name: "Pineapple", price: 2.1 },
-];
-const index = ref(0);
-// const currentFruit = ref(fruitsWithPrice[index.value]);
+]);
 
-const currentFruit = computed(() => fruitsWithPrice[index.value]);
+const fruitName = ref("");
+const fruitPrice = ref(0);
 
-//change the value of index
-const next = () => {
-  index.value = (index.value + 1) % fruitsWithPrice.length;
-  //   currentFruit.value = fruitsWithPrice[index.value];
+const add = () => {
+  fruitsWithPrice.value.push({
+    name: fruitName.value,
+    price: fruitPrice.value,
+  });
+  fruitName.value = "";
+  fruitPrice.value = 0;
 };
 
-//calculate total fruit price
+/*let totalPrice = ref(0);
+for (let i = 0; i < fruitsWithPrice.value.length; i++) {
+  totalPrice.value += fruitsWithPrice.value[i].price;
+}
+
+totalPrice.value = totalPrice.value.toFixed(2); */
+
 const totalPrice = computed(() => {
   let total = 0;
-  for (const fruit of fruitsWithPrice) {
-    total += fruit.price;
+  for (let i = 0; i < fruitsWithPrice.value.length; i++) {
+    total += fruitsWithPrice.value[i].price * 1;
   }
   return total.toFixed(2);
 });
 </script>
 <template>
-  <p class="mb-2">Index: {{ index }}</p>
-  <p class="mb-2">Fruit: {{ currentFruit.name }}</p>
-  <!-- <p class="mb-2">Fruit: {{ fruitsWithPrice[index].name }}</p> -->
-  <p class="mb-2">Price: {{ currentFruit.price }}</p>
-  <p class="mb-2">Total Price: {{ totalPrice }}</p>
-  <!-- <p class="mb-2">Price: {{ fruitsWithPrice[index].price }}</p> -->
+  <p class="mb-10">{{ fruitsWithPrice }}</p>
+  <p class="mt-5">
+    Fruit Name: <input class="text-black" type="text" v-model="fruitName" />
+  </p>
+  <p class="mt-5">
+    Fruit Price: <input class="text-black" v-model="fruitPrice" />
+  </p>
+  <!-- <p class="mt-5">
+    Fruit Price: <input class="text-black" type="number" v-model="fruitPrice" />
+  </p> -->
+  <p class="mt-5">Total: {{ totalPrice }}</p>
   <p class="mt-5">
     <button
-      @click="next()"
+      @click="add()"
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
-      Next Fruit
+      Add Fruit
     </button>
   </p>
 </template>
