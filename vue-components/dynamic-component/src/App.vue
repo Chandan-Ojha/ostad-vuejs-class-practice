d2cadf4fe0754528cfe7f5e1822ee28ddf1b6661
Chandan Ojha <chandanojha776@gmail.com>
2023-10-06 18:39:14 +0600

    `dynamic-component` practice done

---
+++ vue-components/dynamic-component/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { shallowRef } from "vue";
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
+const activeComponent = shallowRef(ComponentA);
+
+function setAActive() {
+  activeComponent.value = ComponentA;
+}
+
+function setBActive() {
+  activeComponent.value = ComponentB;
+}
+</script>
+
+<template>
+  <p>
+    <component :is="activeComponent" />
+  </p>
+  <button
+    @click="setAActive()"
+    class="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Show A
+  </button>
+  <button
+    @click="setBActive()"
+    class="ml-5 mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Show B
+  </button>
+</template>
+
+<style scoped></style>
---
+++ vue-components/dynamic-component/src/components/ComponentB.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>Component B</h1>
+</template>
+
+<style scoped></style>
