b324ecbf3965210abf20e5d5570dcb71edbcf902
Chandan Ojha <chandanojha776@gmail.com>
2023-10-09 12:43:41 +0600

    `multiple-input-from-one-v-model-value`

--- vue-ui-components/component-model/src/App.vue
+++ vue-ui-components/component-model/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref,reactive } from "vue";
+import { ref, reactive } from "vue";
 import Person from "./components/Person.vue";
 import SingleItem from "./components/SingleItem.vue";
 import SingleItemY from "./components/SingleItemY.vue";
 import MultipleItem from "./components/MultipleItem.vue";
 import ButtonItem from "./components/ButtonItem.vue";
+import NameComponent from "./components/NameComponent.vue";
 
 const person = reactive({
   name: "John Doe",
@@ -16,28 +17,58 @@ const person = reactive({
 </script>
 
 <template>
-<section class="mx-auto container">
-  <h1 class="my-10">Component Model</h1>
+  <!-- <section class="mx-auto container"> -->
+  <!-- <h1 class="my-10">Component Model</h1>
   <p class="my-5">
     <h2 class="text-xl">Parent</h2>
     {{ person.name }} is {{ person.age }} years old and works as a {{ person.occupation }}.
-  </p>
+  </p> -->
 
   <!-- <Person :person="person"/> -->
   <!-- <SingleItem v-model="person.name" label="Name"/> -->
 
-  <MultipleItem v-model:name="person.name" v-model:age="person.age" v-model:occupation="person.occupation"/>
+  <!-- <MultipleItem v-model:name="person.name" v-model:age="person.age" v-model:occupation="person.occupation"/> -->
 
-  <hr/>
+  <!-- <hr/> -->
 
   <!-- <SingleItemY v-model:name="person.name" label="Name"/> -->
 
-  <hr/>
+  <!-- <hr/> -->
 
-  <ButtonItem v-model:name="person.name" label="Name"/>
-  <ButtonItem v-model:name="person.occupation" label="Occupation"/>
+  <!-- <ButtonItem v-model:name="person.name" label="Name"/>
+  <ButtonItem v-model:name="person.occupation" label="Occupation"/> -->
 
-</section>
+  <!-- </section> -->
+
+  <div class="container mx-auto">
+    <div class="flex items-center mt-5">
+      <img class="logo" src="./assets/vue.svg" alt="Vue logo" />
+      <h1 class="text-2xl">Component Model</h1>
+    </div>
+
+    <div class="mt-10">Original Name: {{ person.name }}<br /></div>
+
+    <div class="mt-10">
+      <label>Component</label><br /><br />
+      <NameComponent v-model="person.name" />
+    </div>
+  </div>
 </template>
 
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  padding-left: 0;
+  will-change: filter;
+  transition: filter 300ms;
+}
 
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
